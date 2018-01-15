8b4b6754ff1bc27ded7031e2e03d244a5fcc6914
chris-m-cann <canniman@googlemail.com>
2018-01-15 21:50:17 +0000

    initial commit for static_array

---
+++ csb/static_array/static_array.hpp
@@ -0,0 +1,158 @@
+#pragma once
+
+#include <core/concepts.hpp>
+
+#include <iostream>
+
+namespace csb
+{
+    template<auto>
+    struct idx_t{};
+
+    template<ccept::Regular T, std::size_t n>
+    class static_array
+    {
+        public:
+        using value_type = T;
+        using value_t = T;
+        using size_type = std::size_t;
+        using reference = T&;
+        using const_reference = T const&;
+        using pointer = value_type*;
+        using const_pointer = value_type const*;
+        using iterator = T*;
+        using const_iterator = T const*;
+        static constexpr size_type size_v = n;
+
+        constexpr static_array()
+            noexcept(std::is_nothrow_default_constructible_v<value_type>)
+             = default;
+
+        constexpr static_array(static_array  const&) 
+            noexcept(std::is_nothrow_copy_constructible_v<value_type>) = default;
+        constexpr static_array(static_array  &&) 
+            noexcept(std::is_nothrow_move_constructible_v<value_type>) = default;
+        constexpr static_array& operator=(static_array const&) 
+            noexcept(std::is_nothrow_copy_assignable_v<value_type>) = default;
+        constexpr static_array& operator=(static_array &&)
+            noexcept(std::is_nothrow_move_assignable_v<value_type>) = default;
+        
+        ~static_array() 
+            noexcept(std::is_nothrow_destructible_v<value_type>) = default;
+
+        // should I offer this noexpetness in case I end up using lazy initialization??
+        constexpr static_array(value_type const&all)
+            noexcept(
+                std::is_nothrow_default_constructible_v<value_type>
+                && std::is_nothrow_copy_assignable_v<value_type>)
+                : _store{}
+        { fill(all); }
+
+        template<ccept::Range R>
+        constexpr static_array(R const&rng ) : _store{}
+        {
+            init(rng);
+        }
+
+        constexpr static_array(std::initializer_list<T> const&il)
+            noexcept(
+                std::is_nothrow_default_constructible_v<value_type>
+                && std::is_nothrow_copy_assignable_v<value_type>)
+                : _store{}
+        {
+            init(il);
+        }
+
+
+
+        constexpr const_reference operator[](size_type i) const noexcept
+        { return _store[i]; }
+        template<auto i>
+        constexpr const_reference operator[](idx_t<i>) const noexcept
+        { static_assert(i  > 0 && i < n, "out of bounds"); return _store[i]; }
+
+        constexpr reference operator[](size_type i) noexcept
+        { return _store[i];}
+        template<auto i>
+        constexpr reference operator[](idx_t<i>) noexcept
+        { static_assert(i  > 0 && i < n, "out of bounds"); return _store[i]; }
+
+        constexpr const_reference at(size_type i) const
+        { return const_cast<static_array const&>(*this).operator[](i); }
+        constexpr reference at(size_type i)
+        {
+            if (i >= n) throw std::out_of_range("static_array::at() range error");
+            return _store[i];
+        }
+
+        constexpr const_iterator begin() const noexcept
+        { return std::begin(_store); }
+        constexpr const_iterator end() const noexcept
+        { return std::end(_store); }
+
+        constexpr iterator begin() noexcept
+        { return std::begin(_store); }
+        constexpr iterator end() noexcept
+        { return std::end(_store); }
+
+        constexpr reference front() noexcept
+        { return _store[0]; }
+        constexpr const_reference front() const  noexcept
+        { return const_cast<static_array const&>(*this).front(); }
+
+        constexpr reference back() noexcept
+        { return _store[n-1]; }
+        constexpr const_reference back() const noexcept
+        { return const_cast<static_array const&>(*this).back(); }
+
+
+        constexpr bool is_empty() const noexcept
+        { return false; }
+        constexpr bool empty() const noexcept
+        { return is_empty(); }
+
+        constexpr size_type size() const noexcept{ return n; }
+
+        template<ccept::Range R>
+        constexpr void assign(R const& r)
+        {
+            init(r);
+        }
+
+        constexpr void fill(value_type const& with) 
+            noexcept(std::is_nothrow_copy_assignable_v<value_type>)
+        {
+            std::fill_n(this->begin(), n, with);
+        }
+
+        friend bool operator==(
+            static_array const&l,
+            static_array const&r) noexcept
+        {
+            return std::equal(l.begin(), l.end(), r.begin());
+        }
+        friend bool operator!=(
+            static_array const&l,
+            static_array const&r) noexcept
+        {
+            return !(r==l);
+        }
+
+        private:
+        template<ccept::Range R>
+        constexpr void init(R const& rng)
+        {
+            using std::begin;
+            using std::end;
+            auto me = this->begin();
+            for (auto rit = begin(rng); 
+                me != this->end() && rit != end(rng);
+                ++me, ++rit)
+            {
+                *me = *rit;
+            }
+        }
+
+        T _store[n];      
+    };
+}
---
+++ csb/static_array/static_array.test.cpp
@@ -0,0 +1,209 @@
+#include <static_array/static_array.hpp>
+
+#include <core/test_util.hpp>
+#include <core/is_regular_test.hpp>
+
+#include <catch/catch.hpp>
+
+
+
+
+namespace test
+{
+    namespace
+    {
+        template<int i = 3>
+        struct int_generator
+        {
+            using sut_t = csb::static_array<int, i>;
+            using data_t = int;
+
+            sut_t create_sut() const { return {}; }
+            int_data_generator data_source() const { return {}; }
+        };
+        template<int i = 3>
+        struct custom_generator
+        {
+            using sut_t = csb::static_array<custom, i>;
+            using data_t = custom;
+
+            sut_t create_sut() const { return {}; }
+            custom_data_generator data_source() const { return {}; }
+        };
+    }
+
+    template <int size> struct arg_printer<csb::static_array<int, size>>
+    {
+        static std::ostream &print(std::ostream &os,
+                                   csb::static_array<int, size> const &sa)
+        {
+            os << "static_array<int>[";
+            for (auto const &i : sa)
+            {
+                os << i << ",";
+            }
+            os << (sa.is_empty() ? "]" : "\b]");
+            return os;
+        }
+    };
+    
+    template <int size> struct arg_printer<csb::static_array<custom, size>>
+    {
+        static std::ostream &print(std::ostream &os,
+                                   csb::static_array<custom, size> const &sa)
+        {
+            os << "static_array<custom>[";
+            for (auto const &i : sa)
+            {
+                os << i << ",";
+            }
+            os << (sa.is_empty() ? "]" : "\b]");
+            return os;
+        }
+    };
+
+    template <std::size_t size, typename... Args>
+    auto create_sut(Args &&... args)
+    {
+        return csb::static_array<int, size>{std::forward<Args>(args)...};
+    }
+
+
+    auto data_generator() { return int_data_generator(); }
+
+    template <SutGenerator G> auto equality(G g)
+    {
+        // empty list is reflexive and equal to itself and transitive
+        auto empty = g.create_sut();
+        equality_reflexive(empty);
+        auto also_empty = g.create_sut();
+        equality_symetric(empty, also_empty);
+        auto empty_as_well = g.create_sut();
+        equality_transitive(empty, empty_as_well, also_empty);
+
+        // two lists with the same number of elements in the same sequence
+        auto not_empty = g.create_sut();
+        auto also_not_empty = g.create_sut();
+        auto not_empty_as_well = g.create_sut();
+        auto gen = g.data_source();
+        data_t<G> in[] = {gen(), gen(), gen()};
+
+        not_empty.assign(in);
+        also_not_empty.assign(in);
+        not_empty_as_well.assign(in);
+
+
+        // different values are !=
+        // different orders are !=
+        GIVEN_T("two lists with the same size", G)
+        {
+            auto a = g.create_sut();
+            auto b = g.create_sut();
+
+            auto gen = g.data_source();
+            auto v1 = gen();
+            auto v2 = gen();
+            auto v3 = gen();
+
+            WHEN("the values contained are different")
+            {
+                a.fill(v1);
+                b.fill(v2);
+                THEN("they are not equal") { REQUIRE(a != b); }
+            }
+            WHEN("with equal nodes in different orders")
+            {
+                auto order1 = {v1, v2, v3};
+                auto order2 = {v2, v1, v3};
+                a.assign(order1);
+                b.assign(order2);
+                THEN("they are not equal") { REQUIRE(a != b); }
+            }
+        }
+    }
+
+    SCENARIO("static_array: equality")
+    {
+        equality(int_generator<3>{});
+        equality(int_generator<1>{});
+
+        equality(custom_generator<1>{});
+        equality(custom_generator<5>{});
+    }
+
+    using namespace csb;
+    SCENARIO("Range init")
+    {
+        GIVEN("A Range of elements a, b, c")
+        {
+            auto gen = data_generator();
+            auto a = gen();
+            auto b = gen();
+            auto c = gen();
+            decltype(gen()) abc[] = {a, b, c};
+
+            WHEN("Initializing a static_array of size 3")
+            {
+                auto sut = create_sut<3>(abc);
+                THEN("Array has element a, b and c in order")
+                {
+                    REQUIRE(sut[0] == a);
+                    REQUIRE(sut[1] == b);
+                    REQUIRE(sut[2] == c);
+                }
+            }
+            WHEN("Initializing a static_array of size 2")
+            {
+                auto sut = create_sut<2>(abc);
+                THEN("Array has element a, b in order")
+                {
+                    REQUIRE(sut[0] == a);
+                    REQUIRE(sut[1] == b);
+                }
+            }
+            WHEN("Initializing a static_array of size 4")
+            {
+                auto sut = create_sut<4>(abc);
+                THEN("Array has element a, b and c in order")
+                {
+                    REQUIRE(sut[0] == a);
+                    REQUIRE(sut[1] == b);
+                    REQUIRE(sut[2] == c);
+                }
+            }
+        }
+    }
+
+    SCENARIO("operator[]")
+    {
+        constexpr csb::static_array<int, 3> arr{1, 2, 3};
+        constexpr int a = arr[1];
+        static_assert(a == 2, "grr");
+    }
+        
+    SCENARIO("at")
+    {
+        constexpr csb::static_array<int, 3> arr{1, 2, 3};
+        constexpr int a = arr.at(1);
+        static_assert(a == 2, "grr");
+    }
+
+    SCENARIO("fill ctor")
+    {
+        GIVEN("a static_array of size 3")
+        {
+            WHEN("when initializing it with value_type v")
+            {
+                int v = 4;
+                csb::static_array<int, 3> sut(v);
+
+                THEN("sll three elements are of value v")
+                {
+                    REQUIRE(sut[0] == v);
+                    REQUIRE(sut[1] == v);
+                    REQUIRE(sut[2] == v);
+                }
+            }
+        }
+    }
+}
